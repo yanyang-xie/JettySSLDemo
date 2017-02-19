c8d279d49c8e94aa66eeaf4f43c54d92f9009210
zane29 <zhouhaiming0828@gmail.com>
2017-02-19 23:11:16 +0800

    first commit

---
+++ src/main/java/com/chow/jettyssl/ServletContextServer.java
@@ -0,0 +1,31 @@
+package com.chow.jettyssl;
+
+
+import org.eclipse.jetty.http.ssl.SslContextFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Administrator on 2017/2/19.
+ */
+public class ServletContextServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        // 设置ssl连接器
+        SslSocketConnector ssl_connector = new SslSocketConnector();
+        ssl_connector.setPort(9000);
+        SslContextFactory cf = ssl_connector.getSslContextFactory();
+        cf.setKeyStorePath("D:/MyProjects/IdeaProjects/JettySSLDemo/tomcat.keystore");
+        cf.setKeyStorePassword("123456");
+        cf.setKeyManagerPassword("123456");
+        server.addConnector(ssl_connector);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloServlet()), "/HelloServlet");
+        server.start();
+        server.join();
+    }
+}
